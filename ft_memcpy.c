a998e2af0972aa87f5c9abe16cc7ba38d8cc4374
Oluwaseun Sunday Adewumi <Saiahs.ent@gmail.com>
2023-11-02 15:20:50 +0200

    Add files via upload

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,13 @@
 /*   By: oadewumi <oadewumi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 15:12:00 by oadewumi          #+#    #+#             */
-/*   Updated: 2023/11/02 15:12:01 by oadewumi         ###   ########.fr       */
+/*   Updated: 2023/11/02 15:19:10 by oadewumi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string.h>
+
+void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
+{
+	
+}
