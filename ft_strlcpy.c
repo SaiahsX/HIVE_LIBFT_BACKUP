ef601fb0aad2de1481172aa2e7c878a9083c6071
Oluwaseun Sunday Adewumi <Saiahs.ent@gmail.com>
2023-11-09 18:55:27 +0200

    Add files via upload

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: oadewumi <oadewumi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 15:10:21 by oadewumi          #+#    #+#             */
-/*   Updated: 2023/11/07 16:44:29 by oadewumi         ###   ########.fr       */
+/*   Updated: 2023/11/09 18:28:31 by oadewumi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,14 @@
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
-	 
+	size_t	i;
+
+	i = 0;
+	if (dstsize == 0)
+	{
+		while (src[i] != '\0')
+			i++;
+		return (i);
+	}
+	while (i < (dstsize - 1))
 }
