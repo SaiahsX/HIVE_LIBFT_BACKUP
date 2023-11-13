96677d3eda5240f5c899b448f797d61a4afe56da
Oluwaseun Sunday Adewumi <Saiahs.ent@gmail.com>
2023-11-13 18:41:06 +0200

    Add files via upload

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,9 +6,26 @@
 /*   By: oadewumi <oadewumi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 14:51:17 by oadewumi          #+#    #+#             */
-/*   Updated: 2023/11/13 15:25:27 by oadewumi         ###   ########.fr       */
+/*   Updated: 2023/11/13 18:39:23 by oadewumi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*	This function copies src & apppends it at end of dst (concactenation)	*/
+/*	The result is then null terminated.	*/
+/*	The logic of this function is to do ft_strlen on 'src' & 'dst'....	*/
+/*	Also, we need to calculate the remaining space left by the dstsize
+for src to occupy, hence, 'space_for_src = dstsize - dstlen - 1', 
+this we will set as a limiter for the function.	*/
+/*	In this function, I began to utilise my premade function ft_strlen. 	*/
+/*	Also I began to use my header file 'libft.h'	*/
+/*	This meant I had to typecast the source type from const char to char 
+in this function, to make it match the called function variable	*/
+/*	In this function, the return values vary in the sense that if the 
+if the dstlen is larger than dstsize, we get the sum of dstsize & srclen 
+as return values and nothing from source is copied as a result.	*/
+/*	Otherwise, we get the sum of the dstlen & srclen and some copied string	*/
+/*	This function imitates the behaviour of the standard Clibrary function 
+strlcat	*/
 
 #include "libft.h"
 
@@ -16,50 +33,45 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	dstlen;
 	size_t	srclen;
-	size_t	src_index;
+	size_t	src_i;
 	size_t	space_for_src;
+	char	*source;
 
-	dstlen = 0;
-	srclen = 0;
-	while (src[srclen] != '\0')
-	{
-		srclen++;
-	}
-	while (dst[dstlen] != '\0')
-	{
-		dstlen++;
-	}
+	src_i = 0;
+	source = (char *) src;
+	dstlen = ft_strlen(dst);
+	srclen = ft_strlen(source);
 	if (dstlen > dstsize)
 		return (dstsize + srclen);
 	space_for_src = dstsize - dstlen - 1;
-	src_index = 0;
-	while (src[src_index] != '\0' && src_index < space_for_src)
+
+	while (source[src_i] != '\0' && src_i < space_for_src)
 	{
-		dst[dstlen + src_index] = src[src_index];
-		src_index++;
+		dst[dstlen + src_i] = source[src_i];
+		src_i++;
 	}
-	dst[dstlen + src_index] = '\0';
+	dst[dstlen + src_i] = '\0';
 	return (dstlen + srclen);
 }
-
+/*   
 #include <stdio.h>
 #include <string.h>
 int	main(void)
 {
 	char	d[70] = "Hello, you are doing alright";
-	char	s[] = ", yes it is true";
-	int	t = 50;
+	char	s[] = ",_yes it is true";
+	int	t = 32;
 
 	printf("%zu\n", ft_strlcat(d, s, t));
 	printf("%s\n", d);
 
 	char	dx[70] = "Hello, you are doing alright";
-	char	sx[] = ", yes it is true";
-	int		tx = 50;
+	char	sx[] = ",_yes it is true";
+	int		tx = 32;
 
 	int		a = strlcat(dx, sx, tx);
 
 	printf("%d\n", a);
 	printf("%s\n", dx);
 
-}
+} */
--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,9 +6,22 @@
 /*   By: oadewumi <oadewumi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 15:10:21 by oadewumi          #+#    #+#             */
-/*   Updated: 2023/11/13 13:37:39 by oadewumi         ###   ########.fr       */
+/*   Updated: 2023/11/13 17:54:36 by oadewumi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*	This function copies upto 'dstsize -1' characters from 'src' to 
+'dst' and null erminating the result if 'dstsize' is not zero	*/
+/*	it replaces the characters in the source & ends with a null terminator
+which is the reaon for 'dst[i] = '\0'.	*/
+/*	The return values vary iin a sense that if th specified dstsize = 0,
+it returns the lenght of src	*/
+/*	In this function ft_strlcpy, the 'l' indicates its an integer
+return value which is the lenght of the src	*/
+/*	For some unknoownn reason my function still works if the argument size
+dstsize is larger than dst while the original function displays 
+overflow error. Needs more investigation	*/
+/*	This function imitates the behaviour of the standard C library strlcpy	*/
 
 #include "libft.h"
 
@@ -35,22 +48,25 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	}
 	return (i);
 }
-
+/* 
 #include <stdio.h>
+#include <string.h>
 
-int		main(void)
+int	main(void)
 {
 	char	d[] = "12345";
-	
 	const char	s[] = "you can do it!";
-	int a = ft_strlcpy(d, s, 0);
+	int a = ft_strlcpy(d, s, 25);
+
 	printf("%d\n", a);
 	printf("%s\n", d);
-	
-	char	dx[] = "12345";
-	
-	const char	sx[] = "you can do it!";
-	int ax = strlcpy(dx, sx, 0);
-	printf("%d\n", ax);
-	printf("%s\n", dx);
-}
+
+
+	// char	dx[] = "12345";
+	// const char	sx[] = "you can do it!";
+	// int ax = strlcpy(dx, sx, 15);
+
+	// printf("%d\n", ax);
+	// printf("%s\n", dx);
+	// return (0);
+} */
