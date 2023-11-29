2587b5f7038bf50b75f3502c4c3a56317c3191a1
Oluwaseun Sunday Adewumi <Saiahs.ent@gmail.com>
2023-11-29 16:46:37 +0200

    Add files via upload

--- ft_split.c
+++ ft_split.c
@@ -6,9 +6,25 @@
 /*   By: oadewumi <oadewumi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/25 18:28:55 by oadewumi          #+#    #+#             */
-/*   Updated: 2023/11/28 21:01:36 by oadewumi         ###   ########.fr       */
+/*   Updated: 2023/11/29 16:45:43 by oadewumi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*	This function allocates memory and returns an array of strings 
+obtained by splitting the string 's' using the character 'c'as a delimiter.
+The array must also end with a null pointer.	*/
+/*	The function was broken down to 3 parts:
+1:	ft_substr_poll to count through the 's' & return the # of substrings.
+2:	ft_split (primary function) to allocate needed memory & return result.
+3:	ft_free_function to free allocate memory when done.
+4:	ft_filling_substr to print out each substr, using my fxn ft_substr.	*/
+/*	In the ft_filling_substr, we start by eliminating the delimiter &
+we copy each substring into the allocated memory space in spit_res. We use
+the index 'J' to loop through the substrings and important to free the 
+allocated memories if there is a failled memory allocation. Then loop
+through index 'J' only after this.	*/
+/*	The split result at the address 'j' will be null terminated. and 
+returned as our needed output	*/
 
 #include "libft.h"
 
@@ -19,7 +35,6 @@ static size_t	ft_substr_poll(char const *str, char c)
 
 	count = 0;
 	in_substring = 0;
-
 	while (*str)
 	{
 		if (*str && *str != c && in_substring == 0)
@@ -80,55 +95,28 @@ char	**ft_split(char const *s, char c)
 	char	**split_res;
 
 	split_res = (char **)malloc((ft_substr_poll(s, c) + 1) * sizeof(char *));
-
 	if (!split_res)
 		return (0);
-
 	split_res = ft_filling_substr(split_res, s, c);
 	return (split_res);
 }
-
-
-
 #include <stdio.h>
 
 int	main(void)
 {
-	char const	str[] = "lorem ipsum dolor sit amet, 4664649 consectetur adipiscing elit. Sed non risus. Suspendisse";
+	char const	*str;
 	char		d;
 	char		**res;
+	int			i;
 
+	i = 0;
+	str = "  lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse";
 	d = ' ';
 	res = ft_split(str, d);
 	while (*res != 0)
 	{
-		printf("%s\n", *res);
+		printf("Ultra split result[%d] =  %s\n", i, *res);
 		res++;
+		i++;
 	}
 }
-// int    main(void)
-// {
-//     int i = 0;
-//     char **test;
-//     char	*str = "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse";
-//     test = ft_split(str, ' ');
-//     while (i < (int)ft_strlen(str))
-//     {
-//         printf("\nft_split[0]:%s\n", test[i]);
-//         i++;
-//     }
-//     // printf("\nft_split[0]:%s\n", test[0]);
-//     // printf("\nft_split[1]:%s\n", test[1]);
-//     // printf("\nft_split[2]:%s\n", test[2]);
-//     // printf("\nft_split[3]:%s\n", test[3]);
-//     // printf("\nft_split[4]:%s\n", test[4]);
-
-//     // printf("\nft_split[5]:%s\n", test[5]);
-//     // printf("\nft_split[6]:%s\n", test[6]);
-//     // printf("\nft_split[7]:%s\n", test[7]);
-//     // printf("\nft_split[8]:%s\n", test[8]);
-
-//     printf("\nft_split[null]:%s\n", test[i]);
-
-//     return (0);
-// }
