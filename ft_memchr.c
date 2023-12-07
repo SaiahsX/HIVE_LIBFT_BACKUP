f6bd90fa7a29d2596d9de145fda871558bc643c1
Oluwaseun Sunday Adewumi <Saiahs.ent@gmail.com>
2023-12-07 12:38:02 +0200

    Update ft_memchr.c
    Updated the while condition to remove 'str[i]' 
    because it causes leaks.

--- ft_memchr.c
+++ ft_memchr.c
@@ -10,14 +10,16 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-/*This function checks for the first occurence of 
+/*	This function checks for the first occurence of 
 'int c' (converted to an unsigned char)	*/
 /* Its a void function like function ft_bzero but this has a return value 
 because it has a pointer which makes the mains different	*/
 /* The initial 's' declaration was converted to an 'unsigned char str'	*/
-/* This function is similar to ft_strchr*/
+/* 	This function is similar to ft_strchr*/
 /* The return value is a pointer to the byte located (specified by 'c')	*/
-/*In the mains, the function is passed in the print function	*/
+/*	In the mains, the function is passed in the print function	*/
+/*	Updated the while condition to remove 'str[i]' 
+because it causes leaks.	*/
 /* This function imitates the behaviour of the standard
 C library function memchr	*/
 /*	'i' is the string index	*/
@@ -33,7 +35,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	i = 0;
 	g = c;
 	str = (unsigned char *) s;
-	while (str[i] && i < n)
+	while (i < n)
 	{
 		if (str[i] == g)
 			return (str + i);
