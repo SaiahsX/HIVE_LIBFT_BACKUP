26bda466edfd890b992914a356135b8973e72d1b
Oluwaseun Sunday Adewumi <Saiahs.ent@gmail.com>
2023-12-07 14:42:43 +0200

    Update ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -14,34 +14,32 @@
 a result of the conctenation of s1 an s2.	*/
 /*	For this function, the variable types of the strings are typecasted &
 their lenghts determined via ft_strlen	*/
+/*	Removed  above two new variablles to typecast the variables in the parameter.
+This is because ft_strlen got fixed so no need for typecasting.	*/
+/*	The operation of ft_strlcpy and ft_strcat was used for the copying	*/
 /*	Both source lengths + 1 will serve as the count for the memory allocation
 Then make checks if no memory was not allocted and return Null if none	*/
 /*	A new variable 'result' is initalized with the allocate memory	*/
-/*	ft_strlcat was used to concartenate the strings and equated to 'result'	*/
 /*	return value is the pointer to 'result', the new string	*/
 
 #include "libft.h"
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*source;
-	char	*dest;
 	size_t	dest_len;
 	size_t	source_len;
 	char	*result;
 
-	source = (char *)s2;
-	dest = (char *)s1;
 	if (!s1 || !s2)
 		return (NULL);
-	dest_len = ft_strlen(dest);
-	source_len = ft_strlen(source);
-
+	dest_len = ft_strlen(s1);
+	source_len = ft_strlen(s2);
 	result = (char *) malloc ((dest_len + source_len + 1) * sizeof(char));
 	if (!result)
 		return (NULL);
-	result = (char *)ft_strlcat(dest, source, (dest_len + source_len + 1));
-	return (dest);
+	ft_strlcpy(result, s1, dest_len + 1);
+	ft_strlcat(result, s2, (dest_len + source_len + 1));
+	return (result);
 }
 
 /* 
