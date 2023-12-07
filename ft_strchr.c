aa2bb49adbc390b6ccda25cf48ae736f79c9f414
Oluwaseun Sunday Adewumi <Saiahs.ent@gmail.com>
2023-12-07 14:11:48 +0200

    Update ft_strchr.c
    Removed the condition 'while (str[i])'  and introduced a new variable int len that was used to contain the loop within the length of str

--- ft_strchr.c
+++ ft_strchr.c
@@ -14,6 +14,8 @@
 (converted to char by typecasting '(char)c'or just plain converion) 
 in the string pointed to by 'str'.*/
 /* The null terminator character is considered part of the string*/
+/*	Removed the condition 'while (str[i])'  and introduced a new variable 
+int len that was used to contain the loop within ithe lenght of str	*/
 /* The return value is a pointer to the located character or 
 Null if the character does not appear in the string*/
 /* The return value is the first occurence of the character 
@@ -21,16 +23,21 @@ and every other character afterwards */
 /* This function imitates the behaviour of the standard Clibrary
 function strchr*/
 
+#include "libft.h"
+
 char	*ft_strchr(const char *str, int c)
 {
 	int	i;
+	int	len;
 
 	i = 0;
-	while (str[i])
+	len = ft_strlen((char *)str) + 1;
+	while (len > 0)
 	{
 		if (str[i] == (char) c)
 			return ((char *) str + i);
 		i++;
+		len--;
 	}
 	return (0);
 }
