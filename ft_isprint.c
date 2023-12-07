b82ccfc72a8504733112561c5e4a2aeda40a6e90
Oluwaseun Sunday Adewumi <Saiahs.ent@gmail.com>
2023-12-07 12:30:40 +0200

    Update ft_isprint.c
    changed from octal ascii table to decimal ascii table

--- ft_isprint.c
+++ ft_isprint.c
@@ -12,14 +12,14 @@
 
 /*	This function checks if the argument 'p' is a printable character
 from the ASCII table,then it returns a value of 1 if that is true 
-and 0 if that is false.
-*/
+and 0 if that is false.	*/
+/*	Updated my search range from octal to decimal from the ascii table.	*/
 /* This fuction is to imitate the behaviour of the 
 standard C library function 'isalnum'*/
 
 int	ft_isprint(int p)
 {
-	if ((p >= 040 && p <= 077) || (p >= 100 && p <= 114))
+	if (p >= 32 && p <= 126)
 	{
 		return (1);
 	}
