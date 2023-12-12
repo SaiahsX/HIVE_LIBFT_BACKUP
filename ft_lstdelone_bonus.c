e649396eb0d673cb4e8c9c006e9e159e306f8720
Oluwaseun Sunday Adewumi <Saiahs.ent@gmail.com>
2023-12-12 18:14:41 +0200

    Add files via upload

--- ft_lstdelone_bonus.c
+++ ft_lstdelone_bonus.c
@@ -6,7 +6,7 @@
 /*   By: oadewumi <oadewumi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 15:00:45 by oadewumi          #+#    #+#             */
-/*   Updated: 2023/12/12 18:05:08 by oadewumi         ###   ########.fr       */
+/*   Updated: 2023/12/12 18:14:11 by oadewumi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,14 @@ free the node.	*/
 /*	Although the memory of the next must not be freed.	*/
 /*	First of all we need to check if the function 'del' exists.
 If it exists, then we write the function 'del', using parenthesis as (*del),
-(this is a way, we use a typecast concept to the void 'del' function)
-to delete the content of the node 'lst' like this (*del)(lst->content).	*/
+to delete the content of the node 'lst' like this (*del)(lst->content).
+(this is a way, we use a typecast concept to the void 'del' function).
+(In C, function pointers are called using (*functionPointerName)(). 
+It dereferences the function pointer to call the function it points to.
+In this case, (*del)(lst->content) is dereferencing the function pointer del 
+and passing lst->content as an argument to the function that del is pointing to.
+The parentheses around (*del) are used to ensure that the function pointer del
+ is dereferenced before being called as a function.)	*/
 /*	Thus, we free the node 'lst'	*/
 
 #include "libft.h"
